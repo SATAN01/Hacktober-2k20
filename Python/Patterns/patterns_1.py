fcce270d89cd74773305eda192255b5634a3909f
SATAN01 <68592417+SATAN01@users.noreply.github.com>
2020-10-05 12:06:11 +0530

    Update patterns_1.py

--- Python/Patterns/patterns_1.py
+++ Python/Patterns/patterns_1.py
@@ -1,19 +1,20 @@
-n=int(input())
-i=1
-while i<=n:
-    j=1
-    while j<=i:
-        print("*", end=' ')
-        j+=1
-    print()
-    i+=1
+def takinginput():
+    try:
+        global i
+        global n
+        n = int(input())
+        i = 1
+    except Exception:
+        print('Try entering a numeric value')
+        takinginput()   #function will re run if user doesn't enters numeric values
+ 
 
-"""
-input  : 5
-output :
-* 
-* *
-* * *
-* * * *
-* * * * *
-"""
+takinginput()
+
+while i <= n:
+    j = 1
+    while j <= i:
+        print("*", end=' ')
+        j += 1
+    print()
+    i += 1
